28b561685cf3b4e77655e4fc2482bc3bd594a10a
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2022-06-08 05:36:08 -0400

    reformat parts of the code

--- Assets/Scripts/ECS/Systems/CollisionSystem.cs
+++ Assets/Scripts/ECS/Systems/CollisionSystem.cs
@@ -20,23 +20,23 @@ public class CollisionSystem : SystemBase
                     pi.position = new float3(box.center.x - (box.size.x/2) - EPS, pi.position.y, pi.position.z);
                     pi.velocity *= BOUND_DAMPING;
                 }
+                if(pi.velocity.x < 0 && box.isColliding(new float3(pi.position.x - EPS, pi.position.y, pi.position.z))){
+                    pi.position = new float3(box.center.x + (box.size.x/2) + EPS, pi.position.y, pi.position.z);
+                    pi.velocity *= BOUND_DAMPING;
+                }
                 if(pi.velocity.y > 0 && box.isColliding(new float3(pi.position.x, pi.position.y + EPS, pi.position.z))){
                     pi.position = new float3(pi.position.x, box.center.y - (box.size.y/2) - EPS, pi.position.z);
+                    pi.velocity *= BOUND_DAMPING;
+                }               
+                if(pi.velocity.y < 0 && box.isColliding(new float3(pi.position.x, pi.position.y - EPS, pi.position.z))){
+                    pi.position = new float3(pi.position.x, box.center.y + (box.size.y/2) + EPS, pi.position.z);
                     pi.velocity *= BOUND_DAMPING;
                 }
                 if(pi.velocity.z > 0 && box.isColliding(new float3(pi.position.x, pi.position.y, pi.position.z + EPS))){
                     pi.position = new float3(pi.position.x, pi.position.y, box.center.z - (box.size.z/2) - EPS);
                     pi.velocity *= BOUND_DAMPING;
                 }
-                if(pi.velocity.x < 0 && box.isColliding(new float3(pi.position.x - EPS, pi.position.y, pi.position.z))){
-                    pi.position = new float3(box.center.x + (box.size.x/2) + EPS, pi.position.y, pi.position.z);
-                    pi.velocity *= BOUND_DAMPING;
-                }
-                if(pi.velocity.y < 0 &&box.isColliding(new float3(pi.position.x, pi.position.y - EPS, pi.position.z))){
-                    pi.position = new float3(pi.position.x, box.center.y + (box.size.y/2) + EPS, pi.position.z);
-                    pi.velocity *= BOUND_DAMPING;
-                }
-                if(pi.velocity.z < 0 &&box.isColliding(new float3(pi.position.x, pi.position.y, pi.position.z - EPS))){
+                if(pi.velocity.z < 0 && box.isColliding(new float3(pi.position.x, pi.position.y, pi.position.z - EPS))){
                     pi.position = new float3(pi.position.x, pi.position.y, box.center.z + (box.size.z/2) + EPS);
                     pi.velocity *= BOUND_DAMPING;
                 }
